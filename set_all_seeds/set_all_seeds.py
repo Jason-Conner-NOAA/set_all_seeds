30b5237b3efc850b55dad9ffe137da6c9e43fa8d
jason-conner-NOAA <jason.conner@noaa.gov>
2026-04-21 12:31:19 -0700

    hardcode seed

--- set_all_seeds/set_all_seeds.py
+++ set_all_seeds/set_all_seeds.py
@@ -1,11 +1,11 @@
-def set_all_seeds(generate=False):
+def set_all_seeds(set_seed=42, generate=False):
     import random
     import numpy as np
     import tensorflow as tf
     if generate:
         seed = random.randint(0, 2**32 - 1)
     else: 
-        seed = 42
+        seed = set_seed
     np.random.seed(seed)
     random.seed(seed)
     tf.random.set_seed(seed)
